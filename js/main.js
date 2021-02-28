1f5bf814df2721c79dc16902ab0e2716f880addf
Dennis Campos <dennisjr89@gmail.com>
2021-02-27 22:13:29 -0500

    updated the pokemon API website. Added new details and artwork along with small CSS stylings

--- js/main.js
+++ js/main.js
@@ -11,8 +11,14 @@ function getPokemon() {
             console.log(data.name)
 
             // Picture of Pokemon - Front Sprite
-            document.querySelector("img").src = data.sprites.front_default
+            document.querySelector(".front_image").src = data.sprites.front_default
+            document.querySelector(".back_image").src = data.sprites.back_default
+            
 
+            // Pokemon height & weight
+            document.querySelector('#pokeHeight').innerText = data.height
+            document.querySelector('#pokeWeight').innerText = data.weight
+            
             // Pokemon Base Experience Points
             document.querySelector('#baseExperience').innerText = data.base_experience 
 
@@ -22,9 +28,16 @@ function getPokemon() {
             // Pokemon Ability #2
             document.querySelector('#abilityTwo').innerText = data.abilities[1].ability.name.toUpperCase()
 
+            // Pokemon official artwork
+            document.querySelector('.official_artwork').src = data.sprites.other['official-artwork'].front_default
+
+            
+            console.log(data.sprites.other['official-artwork'].front_default)
+
 
         })
         .catch(err => {
             console.log(`error ${err}`)
+            alert('Try again, pokemon not found :(')
         })
 }
